60fd8d1b52916730773eed23bdf662d4b3befff8
moyacba <ezequielm010@gmail.com>
2024-10-04 16:11:24 -0300

    fix port in app.js

--- src/app.js
+++ src/app.js
@@ -29,10 +29,10 @@ app.use("/api", router);
 // Middleware para manejar errores
 app.use(handleErrors);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Servidor corriendo en el puerto ${PORT}`);
+// });
 
 // export default app;
 
