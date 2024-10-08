df56b68b1ed6c1758310fc176013ac928f60a8ed
moyacba <ezequielm010@gmail.com>
2024-10-08 04:39:03 -0300

    fix cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "https://modotecnoweb.netlify.app",
+    origin: "https://modotecnoweb.netlify.app/",
     credentials: true,
   })
 );
